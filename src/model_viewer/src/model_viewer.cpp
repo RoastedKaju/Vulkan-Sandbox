f191bda5f6b55dfc03a46156c56a6ef3579be608
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2026-06-25 19:35:20 +0500

    feat: mesh data loading

--- src/model_viewer/src/model_viewer.cpp
+++ src/model_viewer/src/model_viewer.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <array>
 #include <cassert>
+#include <unordered_map>
+#include <filesystem>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
 #include <vulkan/vulkan.h>
@@ -9,8 +11,15 @@
 #include <volk.h>
 #define VMA_IMPLEMENTATION
 #include <vma/vk_mem_alloc.h>
-
-#include "glm/vec2.hpp"
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <tiny_gltf.h>
 
 constexpr bool kHasValidationLayer = true;
 const std::vector<const char *> validation_layers = {"VK_LAYER_KHRONOS_validation"};
@@ -57,6 +66,118 @@ std::vector<VkSemaphore> render_complete_semaphores;
 
 VmaAllocation vertex_buffer_allocation{VK_NULL_HANDLE};
 VkBuffer vertex_buffer{VK_NULL_HANDLE};
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 uv;
+};
+
+struct MeshData {
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+};
+
+static const uint8_t *get_accessor_data(const tinygltf::Model &model, const tinygltf::Accessor &accessor) {
+    const auto &view = model.bufferViews[accessor.bufferView];
+    const auto &buffer = model.buffers[view.buffer];
+
+    return buffer.data.data() + view.byteOffset + accessor.byteOffset;
+}
+
+static MeshData load_mesh_data(const tinygltf::Model &model, const tinygltf::Primitive &primitive) {
+    MeshData mesh;
+
+    const auto pos_it = primitive.attributes.find("POSITION");
+    if (pos_it == primitive.attributes.end()) {
+        throw std::runtime_error("Primitive has no POSITION attribute.");
+    }
+
+    const auto norm_it = primitive.attributes.find("NORMAL");
+    const auto uv_it = primitive.attributes.find("TEXCOORD_0");
+
+    const auto &pos_accessor = model.accessors[pos_it->second];
+
+    const auto *positions = reinterpret_cast<const float *>(get_accessor_data(model, pos_accessor));
+
+    const float *normals = nullptr;
+    const float *uvs = nullptr;
+
+    if (norm_it != primitive.attributes.end()) {
+        normals = reinterpret_cast<const float *>(get_accessor_data(model, model.accessors[norm_it->second]));
+    }
+    if (uv_it != primitive.attributes.end()) {
+        uvs = reinterpret_cast<const float *>(get_accessor_data(model, model.accessors[uv_it->second]));
+    }
+
+    mesh.vertices.resize(pos_accessor.count);
+
+    for (size_t i = 0; i < pos_accessor.count; i++) {
+        Vertex v{};
+
+        v.position = {
+            positions[i * 3 + 0],
+            positions[i * 3 + 1],
+            positions[i * 3 + 2]
+        };
+
+        if (normals) {
+            v.normal = {
+                normals[i * 3 + 0],
+                normals[i * 3 + 1],
+                normals[i * 3 + 2],
+            };
+        }
+
+        if (uvs) {
+            v.uv = {
+                uvs[i * 2 + 0],
+                uvs[i * 2 + 1],
+            };
+        }
+
+        mesh.vertices[i] = v;
+    }
+
+    if (primitive.indices >= 0) {
+        const auto &index_accessor = model.accessors[primitive.indices];
+
+        const uint8_t *index_data = get_accessor_data(model, index_accessor);
+
+        mesh.indices.resize(index_accessor.count);
+
+        switch (index_accessor.componentType) {
+            case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE: {
+                const auto *src = reinterpret_cast<const uint8_t *>(index_data);
+                for (auto i = 0; i < index_accessor.count; i++) {
+                    mesh.indices[i] = src[i];
+                }
+                break;
+            }
+
+            case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT: {
+                const auto *src = reinterpret_cast<const uint16_t *>(index_data);
+                for (auto i = 0; i < index_accessor.count; i++) {
+                    mesh.indices[i] = src[i];
+                }
+                break;
+            }
+
+            case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT: {
+                const auto *src = reinterpret_cast<const uint32_t *>(index_data);
+                for (auto i = 0; i < index_accessor.count; i++) {
+                    mesh.indices[i] = src[i];
+                }
+                break;
+            }
+
+            default:
+                throw std::runtime_error("Unsupported index type.");
+        }
+    }
+
+    return mesh;
+}
 
 static void check(const VkResult result) {
     if (result != VK_SUCCESS) {
@@ -266,7 +387,122 @@ int main(int argc, char *argv[]) {
     std::printf("Swap chain images created.\n");
 
     // depth-attachment
+    std::vector<VkFormat> depth_format_list{VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT};
+    VkFormat depth_format{VK_FORMAT_D32_SFLOAT};
+    for (VkFormat &format: depth_format_list) {
+        VkFormatProperties2 format_properties{.sType = VK_STRUCTURE_TYPE_FORMAT_PROPERTIES_2};
+        vkGetPhysicalDeviceFormatProperties2(devices[device_index], format, &format_properties);
+        if (format_properties.formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) {
+            depth_format = format;
+            break;
+        }
+    }
 
+    assert(depth_format != VK_FORMAT_UNDEFINED && "Depth format is undefined.\n");
+    VkImageCreateInfo depth_image_create_info{
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = depth_format,
+        .extent{
+            .width = static_cast<uint32_t>(window_size.x), .height = static_cast<uint32_t>(window_size.y), .depth = 1
+        },
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = VK_SAMPLE_COUNT_1_BIT,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+        .usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED
+    };
+    VmaAllocationCreateInfo allocation_create_info{
+        .flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO
+    };
+    check(vmaCreateImage(
+        allocator,
+        &depth_image_create_info,
+        &allocation_create_info,
+        &depth_image,
+        &depth_image_allocation, nullptr));
+    VkImageViewCreateInfo depth_view_create_info{
+        .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .image = depth_image,
+        .viewType = VK_IMAGE_VIEW_TYPE_2D,
+        .format = depth_format,
+        .subresourceRange{.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT, .levelCount = 1, .layerCount = 1}
+    };
+    check(vkCreateImageView(device, &depth_view_create_info, nullptr, &depth_image_view));
+    std::printf("Depth image created.\n");
+
+    // mesh
+    tinygltf::TinyGLTF loader;
+    tinygltf::Model model;
+
+    std::string warnings{};
+    std::string errors{};
+
+    const std::filesystem::path model_path{"cube.glb"};
+
+    if (!std::filesystem::exists(model_path)) {
+        std::printf("Model path is invalid.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (model_path.extension() == ".glb") {
+        check(loader.LoadBinaryFromFile(&model, &errors, &warnings, model_path.string()));
+    } else {
+        check(loader.LoadASCIIFromFile(&model, &errors, &warnings, model_path.string()));
+    }
+
+    if (!warnings.empty()) {
+        std::printf("Warning: %s\n", warnings.c_str());
+    }
+    if (!errors.empty()) {
+        std::printf("Errors: %s\n", errors.c_str());
+    }
+
+    tinygltf::Mesh &mesh = model.meshes[0];
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+
+    for (const auto &primitive: mesh.primitives) {
+        MeshData mesh_data = load_mesh_data(model, primitive);
+
+        auto vertex_offset = static_cast<uint32_t>(vertices.size());
+
+        vertices.insert(vertices.end(), mesh_data.vertices.begin(), mesh_data.vertices.end());
+
+        for (uint32_t index: mesh_data.indices) {
+            indices.push_back(index + vertex_offset);
+        }
+    }
+
+    VkDeviceSize vertex_buf_size = sizeof(Vertex) * vertices.size();
+    VkDeviceSize index_buf_size = sizeof(uint32_t) * indices.size();
+
+    VkBufferCreateInfo buffer_create_info{
+        .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .size = vertex_buf_size,
+        .usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT
+    };
+    VmaAllocationCreateInfo vertex_allocation_create_info{
+        .flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT |
+                 VMA_ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT |
+                 VMA_ALLOCATION_CREATE_MAPPED_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO
+    };
+    VmaAllocationInfo vertex_buffer_allocation_info{};
+    check(vmaCreateBuffer(
+        allocator,
+        &buffer_create_info,
+        &vertex_allocation_create_info,
+        &vertex_buffer,
+        &vertex_buffer_allocation,
+        &vertex_buffer_allocation_info));
+    memcpy(vertex_buffer_allocation_info.pMappedData, vertices.data(), vertex_buf_size);
+    memcpy(static_cast<char *>(vertex_buffer_allocation_info.pMappedData) + vertex_buf_size,
+           indices.data(),
+           index_buf_size);
+    std::printf("Mesh data loaded.\n");
 
     return EXIT_SUCCESS;
 }
